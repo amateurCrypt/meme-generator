b5a28edb635983e341da40c2e486d1ad1d8b556d
moh <muhammad.qasim1@live.com>
2023-03-10 10:48:54 -0500

    added a placeholder meme image temporarily to avoid error

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,7 +3,7 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
 
     function buttonClick() {
         const memesArray = memesData.data.memes;
